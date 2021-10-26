de77a91445f8aba8bf0e4ff909efc530f6ba61a2
unknown <leevi.sallantaus@edu.keuda.fi>
2021-10-26 10:29:11 +0300

    Tehtavat

---
+++ StringNimi/StringNimi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringNimi
+{
+    public class Demo
+    {
+        public static void Main()
+        {
+            string name;
+            Console.WriteLine("Kirjoita Nimesi: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name );
+        }
+    }
+}
