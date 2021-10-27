696014fd3d3997ba9556496e10da19b0dddeebb2
Leevi12 <leevi.sallantaus@gmail.com>
2021-10-27 19:41:27 +0300

    Harjoituksia

---
+++ Harjoituksia4/Harjoituksia4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Harjoituksia4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tehtävä1
+            int Luku1;
+            Console.WriteLine("Syötä ikäsi");
+            Luku1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Luku1 + " Näytät ikäistäsi nuoremmalta");
+
+
+            //Tehtävä2
+            int Luku2;
+            Console.WriteLine("Syötä 10 suurempi luku");
+            Luku2 = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i <= Luku2; i++) 
+            {
+                Console.Write(Luku2 + " ");
+            }
+            Console.WriteLine("\n");
+
+
+            //Tehtävä3
+            string yourWord;
+            Console.WriteLine("Syötä jokin sana");
+            yourWord = Console.ReadLine();
+            int len = yourWord.Length;
+            Console.WriteLine( yourWord[len - 1] + yourWord.Substring(1, len - 2) + yourWord[0]);
+        }
+    }
+}
