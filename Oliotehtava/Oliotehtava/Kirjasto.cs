ba87f06d6953dc810e7b76746cc4e210f6904610
Leevi12 <leevi.sallantaus@gmail.com>
2021-11-05 13:44:35 +0200

    Oliotehtava

---
+++ Oliotehtava/Oliotehtava/Kirjasto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oliotehtava
+{
+    class Kirjasto
+    {
+        string bookname;
+        int pagescount;
+        string author;
+        int quantity;
+
+        public Kirjasto(string bname, int pages, string atr, int quan)
+        {
+            bookname = bname;
+            pagescount = pages;
+            author = atr;
+            quantity = quan;
+        }
+        public void ShowQuantityOfBooks()
+        {
+            Console.WriteLine(quantity);
+        }
+
+        public void LoanBook(int loaned)
+        {
+            if (loaned > quantity || quantity == 0)
+            {
+                Console.WriteLine("Sorry we currently don't have enough copies of this book");
+            }
+            else
+            {
+                quantity -= loaned;
+            }
+        }
+        public void EndLoan(int loaned)
+        {
+            if (quantity < 3)
+            {
+                quantity += loaned;
+            }
+            else
+            {
+                Console.WriteLine("Don't try to give us extra books");
+            }
+        }
+    }
+}
---
+++ Oliotehtava/Oliotehtava/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Oliotehtava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kirjasto firstbook = new Kirjasto("Metallica", 440, "Joel McIver", 3);
+            Kirjasto secondbook = new Kirjasto("Arnold Schwarzenegger Total Recall", 423, "Arnold Schwarzenegger & Peter Petre", 3);
+            Kirjasto thirdbook = new Kirjasto("Arsène Lupin, Mestarivarkaan kootut kertomukset", 554, "Maurice LeBlanc", 3);
+
+            firstbook.ShowQuantityOfBooks();
+            firstbook.LoanBook(1);
+            firstbook.ShowQuantityOfBooks();
+            firstbook.EndLoan(1);
+            firstbook.ShowQuantityOfBooks();
+            secondbook.LoanBook(7);
+            thirdbook.EndLoan(2);
+
+        }
+    }
+}
